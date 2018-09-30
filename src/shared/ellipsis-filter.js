57d21cb9cab09bf2b0fc617f36002e753173f2e6
John Papa <john@johnpapa.net>
2018-09-30 13:57:09 -0400

    ellipsis guard logic

--- src/shared/ellipsis-filter.js
+++ src/shared/ellipsis-filter.js
@@ -1,4 +1,6 @@
 export default function(text, maxlength) {
+  if (!text || text.length === 0) return '';
+
   const shortText = text.substring(0, maxlength);
   const dots = shortText.length < text.length ? '...' : '';
   return `${shortText}${dots}`;
